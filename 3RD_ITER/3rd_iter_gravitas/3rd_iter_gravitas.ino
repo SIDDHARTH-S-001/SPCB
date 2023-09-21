4895b9b738307111f033044411f668c9f5da92d1
SIDDHARTH-S-001 <73553742+SIDDHARTH-S-001@users.noreply.github.com>
2023-09-21 16:26:39 +0530

    Add files via upload

---
+++ 3RD_ITER/3rd_iter_gravitas/3rd_iter_gravitas.ino
@@ -0,0 +1,233 @@
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiServer.h>
+
+const char* ssid = "Micromax-HS2";
+const char* password = "Micromax";
+
+WiFiClient client;
+WiFiServer server(80);
+String data = "";
+
+#define pwm_drive 16
+#define dir_drive 13
+#define pwm_brush 18
+#define dir_brush 17
+#define lin_act_1_pwm 33
+#define lin_act_1_dir 32
+#define voltage_pin 19 // Can be used 21, 22
+// #define lin_act_2_pwm 19
+// #define lin_act_2_dir 23
+
+int rpm = 0;
+int pos = 0;                   // in (mm)
+int speed_pwm = 255;           // pwm value (0-255)
+float reference_volt = 11.0;   // reference voltage for which pwm 255 is calibrated to
+float volt = 0.0;              // voltage reading from voltage sensor
+float volt_scale_factor = 1.0;
+int distance = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(dir_drive, OUTPUT);
+  pinMode(pwm_drive, OUTPUT);
+  pinMode(dir_brush, OUTPUT);
+  pinMode(pwm_brush, OUTPUT);
+  pinMode(lin_act_1_pwm, OUTPUT);
+  pinMode(lin_act_1_dir, OUTPUT); 
+  pinMode(voltage_pin, INPUT);
+// pinMode(lin_act_2_pwm, OUTPUT);
+// pinMode(lin_act_2_dir, OUTPUT);  
+
+
+  Serial.begin(115200);
+  Serial.println("Connecting to WIFI");
+  WiFi.begin(ssid, password);
+  while ((!(WiFi.status() == WL_CONNECTED))){
+    delay(500);
+    Serial.print("...");
+  }
+  Serial.println("WiFi connected");
+  Serial.println("ESP32 Local IP is : ");
+  Serial.print((WiFi.localIP()));
+  server.begin();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // int volt = analogRead(v_sensor);
+  // float volt = map(analogRead(voltage_pin), 0, 4096, 0, 25);
+  volt = map(analogRead(voltage_pin), 0, 4095, 0, 25);
+  volt = volt * volt_scale_factor;
+  speed_pwm = (volt/reference_volt)*255;
+
+  client = server.available();
+  if (!client){
+    return; 
+    }
+
+  data = checkClient();
+  String dataVal = data.substring(1, data.length());
+  String rt = data.substring(0, 1); 
+  Serial.println(data); 
+
+  if (rt == "f"){
+    Serial.println("Forward");
+    rpm = dataVal.toInt();
+    forward(rpm);
+  }
+  else if (rt == "r"){
+    Serial.println("Reverse");
+    rpm = dataVal.toInt();
+    reverse(rpm);
+  }
+  else if (rt == "s"){
+    Serial.println("Stop");
+    brake();
+  }
+
+  else if (rt == "c"){
+    Serial.println("Brush Forward");
+    rpm = dataVal.toInt();
+    run_brush(rpm);
+  }
+
+  else if (rt == "e"){
+    Serial.println("Brush Reverse");
+    rpm = dataVal.toInt();
+    run_brush_rev(rpm);
+  }
+
+  else if ((rt == "s")){
+    Serial.println("Stop Brush");
+    stop_brush();
+  }
+  if (rt == "l"){
+      Serial.println("Actuator moved to reference 2 cm stroke");
+      distance = dataVal.toInt();  // 20
+      if (pos > distance){
+        move_actr_down(distance);          
+      }
+      else {
+        move_actr_up(distance); 
+      }
+    }
+
+  if (rt == "m"){
+      Serial.println("Actuator moved to 3 cm stroke: No Cleaning");
+      distance = dataVal.toInt();  // 20
+      if (pos > distance){
+          move_actr_down(distance);          
+        }
+        else {
+          move_actr_up(distance); 
+        }
+      }
+
+  if (rt == "n"){
+    Serial.println("Actuator moved to 1.5 cm stroke: Mild Cleaning");
+    distance = dataVal.toInt();  // 20
+      if (pos > distance){
+        move_actr_down(distance);          
+      }
+      else {
+        move_actr_up(distance); 
+      }
+    }
+  if (rt == "o"){
+      Serial.println("Actuator moved to 1 cm stroke: Deep Cleaning");
+      distance = dataVal.toInt();  // 20
+      if (pos > distance){
+        move_actr_down(distance);          
+      }
+      else {
+        move_actr_up(distance); 
+      }
+    }
+
+  if (rt == "p"){
+      Serial.println("Linear Stop");
+      lin_stop();
+    }
+}
+
+void forward(int rpm){
+      analogWrite(pwm_drive, rpm);
+      digitalWrite(dir_drive, HIGH);
+}
+
+void reverse(int rpm){
+      analogWrite(pwm_drive, rpm);
+      digitalWrite(dir_drive, LOW);
+}
+
+void brake(){
+      analogWrite(pwm_drive, 0);
+}
+
+void run_brush(int rpm){
+      analogWrite(pwm_brush, rpm);
+      digitalWrite(dir_brush, HIGH);
+}
+
+void run_brush_rev(int rpm){
+      analogWrite(pwm_brush, rpm);
+      digitalWrite(dir_brush, LOW);
+}
+
+void stop_brush(){
+      analogWrite(pwm_brush, 0);
+}
+
+void move_actr_up(int distance){
+  Serial.print("moving up ");
+  pos = pos + distance;
+  lin_inc(calc_time(distance));
+}
+
+void move_actr_down(int distance){
+  Serial.print("moving down ");
+  pos = pos - distance;
+  lin_dec(calc_time(distance));
+}
+
+void lin_inc(int dly){
+  analogWrite(lin_act_1_pwm, speed_pwm);
+  digitalWrite(lin_act_1_dir, HIGH);
+  delay(dly);
+  lin_stop();
+}
+
+void lin_dec(int dly){
+  analogWrite(lin_act_1_pwm, speed_pwm);
+  digitalWrite(lin_act_1_dir, LOW);
+  delay(dly);
+  lin_stop();
+}
+
+void lin_stop(){
+  analogWrite(lin_act_1_pwm, 0);
+}
+
+int calc_time(int distance){
+  return (int)(1000*distance/8.359);
+}
+
+int calc_pwm(float volt){
+  // Calibration reference voltage = 13.34V, measured speed = 8.359 mm/s
+  // implementation reference voltage = 11V, calculated proportional speed = 6.893 mm/s - corresponds to 255 PWM
+  speed_pwm = (11/volt)*(255);
+  return speed_pwm;
+}
+
+String checkClient (void){
+  while(!client.available()){
+  delay(1); 
+  Serial.println(".");}
+  Serial.println("Client Found");
+  String request = client.readStringUntil('\r');   // http://{ nodemcu_local_IP }/{ route }{ value } | Eg:-  http://192.168.4.1/l30
+  request.remove(0, 5);  //    192.168.4.1/l30
+  request.remove(request.length()-9,9);  // l30 = route value
+  return request; 
+}
